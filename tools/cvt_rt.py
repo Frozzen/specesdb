1eaf78373c127e75ab6af1b95a31d03491a0d776
Frozzen <ya.goncharo2009@yandex.ru>
2019-01-09 17:17:49 +0700

    импортировал словарь 1

--- tools/cvt_rt.py
+++ tools/cvt_rt.py
@@ -3,6 +3,7 @@
 конвертеры исходных данных база картинок РТ
 - исправить имена файлов
 """
+import json
 import os
 import sys
 
@@ -20,29 +21,45 @@ def convert_rt(fpath):
         cnt = 0
         for file in files:
             fname, ext = os.path.splitext(file)
-            fto =  "%s-%s%s" % (dirn, str(cnt), ext.lower())
+            fto = "%s-%s%s" % (dirn, str(cnt), ext.lower())
             print(len(path) * u'---', file, '\t\t',fto)
             os.chdir(root)
             os.rename(file, fto)
             cnt = cnt+1
 
 
-def convert_kosoburov_lek_syr(fname):
+def convert_kosoburov_lek_syr(fname, outf):
     """
     конвертировать тексты в json
     :param fname:
     :return:
     """
-    fp = open(fname, 'rt')
-    for ln in fp:
-        pass
-    fp.close()
+    result = []
+    item = {}
+    state = 'start'
+    with open(fname, 'rt') as fp:
+        for ln in fp:
+            ln = ln.rstrip()
+            if state == 'start':
+                item = {"tib_name": ln}
+                state = 'property'
+            elif state == 'property':
+                if ln == "":
+                    state = 'start'
+                    result.append(item)
+                else:
+                    iv = ln.split(':')
+                    item[iv[0]] = iv[1]
+
+    with open(outf, 'w') as fp:
+        json.dump(result, fp, ensure_ascii=False)
 
 
 def main(argv):
     #convert_rt('~/work/spicedb/Растительное сырье/тибетская классификация')
     #convert_rt('~/work/spicedb/Растительное сырье/русская классификация')
-    convert_kosoburov_lek_syr('../text/Кособуров-сырье-растительное.txt')
+    convert_kosoburov_lek_syr('text/Кособуров-сырье-растительное.txt',
+                              'text/kosoburov-rast.json')
 
 if __name__ == '__main__':
     main(sys.argv)
